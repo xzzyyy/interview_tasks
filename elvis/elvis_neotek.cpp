4c34707d6b997e3665f4669514030eda787bdbc9
Pavel S <xzzyyy1@gmail.com>
2023-01-10 18:07:20 +0000

    elvis unit tests

--- elvis/elvis_neotek.cpp
+++ elvis/elvis_neotek.cpp
@@ -3,191 +3,147 @@
 // - что, если один разделитель это подстрока другого разделителя, какой из них должен иметь приоритет?
 // В этих случаях я считаю, что любое поведение правильное
 
-#include <vector>
-#include <map>
+#include "elvis_neotek.hpp"
 #include <iostream>
-#include <fstream>
-#include <filesystem>
+#include <thread>
 #include <future>
 using namespace std;
 
 // -----Parser-----------------------------------------------------------
-class Parser
+void Parser::add_text(const std::string& text)
 {
-	mutex mtx;
-	string text;
-	map<size_t, string> positions;
-public:
-	Parser() = default;
-	void add_separator(const string& sep);
-	vector<string> parse();
-	void process_file(const filesystem::directory_entry de, ofstream& out_f);
-	void parallel_process(const filesystem::path &in_folder, ofstream &out_f);
-};
+    this->text = text;
+}
 
-void Parser::add_separator(const string& sep)
+void Parser::add_sep(const std::string& sep)
 {
-	size_t pos = text.find(sep);
-	if (pos != string::npos)
-		positions[pos] = sep;
+    separators.push_back(sep);
 }
 
 vector<string> Parser::parse()
 {
-	vector<string> out;
-	size_t cur = 0;
-	size_t len = text.length();
-	while (cur < len)
-	{
-		auto pos_sep = positions.begin();
-		if (pos_sep == positions.end())
-		{
-			out.push_back(text.substr(cur));
-			cur = len;
-		}
-		else
-		{
-			size_t old_pos = pos_sep->first;
-			string sep = pos_sep->second;
+    map<size_t, string> sep_pos_dict;
+    vector<string> res;
 
-			out.push_back(text.substr(cur, old_pos - cur));
-			cur = old_pos + sep.length();
+    for (const string& sep : separators)
+    {
+        size_t pos = text.find(sep);
+        if (pos != string::npos)
+            sep_pos_dict[pos] = sep;
+    }
 
-			size_t new_pos = text.find(sep, cur);
-			positions.erase(pos_sep);
-			if (new_pos != string::npos)
-			{
-				positions[new_pos] = sep;
-			}
-		}
-	}
-	return out;
-}
+    size_t cur = 0;
+    size_t len = text.length();
+    while (cur < len)
+    {
+        auto ps = sep_pos_dict.begin();
+        if (ps == sep_pos_dict.end())
+        {
+            res.push_back(text.substr(cur));
+            cur = len;
+        }
+        else
+        {
+            size_t old_pos = ps->first;
+            string sep = ps->second;
 
-void Parser::process_file(const filesystem::directory_entry de, ofstream& out_f)
-{
-	{
-		lock_guard<mutex> lg(mtx);
-		cout << "thread " << this_thread::get_id() << " started\n";
-	}
+            res.push_back(text.substr(cur, old_pos - cur));
+            cur = old_pos + sep.length();
 
-	if (!de.is_regular_file())
-		return;
-
-	ifstream in_f(de.path());
-	if (!in_f)
-	{
-		lock_guard<mutex> lg(mtx);
-		cout << "ERROR: can't open file for reading: " << de;
-		return;
-	}
-
-	getline(in_f, text);
-
-	string s;
-	while (in_f)
-	{
-		getline(in_f, s);
-		add_separator(s);
-	}
-
-	vector<string> res = parse();
-	{
-		lock_guard<mutex> lg(mtx);
-
-		out_f << "[" << de.path().filename() << "]:\n";
-		for (const string& str : res)
-			out_f << str << "\n";
-
-		cout << "thread " << this_thread::get_id() << " finished\n";
-	}
-}
-
-void Parser::parallel_process(const filesystem::path &in_folder, ofstream &out_f)
-{
-	unsigned thr_num = thread::hardware_concurrency();
-	vector<future<void>> tasks(thr_num);
-
-	auto file_iter = filesystem::directory_iterator(in_folder);
-	auto file_iter_end = end(filesystem::directory_iterator(in_folder));
-
-	bool started = false;
-	int running = 0;
-	while (!started || running > 0)
-	{
-		for (unsigned i = 0; i < thr_num; ++i)
-		{
-
-			if (!tasks[i].valid())
-			{
-				if (file_iter != file_iter_end)
-				{
-					tasks[i] = async(launch::async, &process_file, this, *file_iter, ref(out_f));
-					++file_iter;
-					++running;
-					started = true;
-				}
-			}
-			else
-			{
-				if (tasks[i].wait_for(0ms) == future_status::ready)
-				{
-					if (file_iter != file_iter_end)
-					{
-						tasks[i] = async(launch::async, &process_file, this, *file_iter, ref(out_f));
-						++file_iter;
-					}
-					else
-					{
-						tasks[i] = future<void>();
-						--running;
-					}
-				}
-				else
-				{
-
-				}
-			}
-		}
-
-		this_thread::sleep_for(100ms);
-	}
-
-	cout << "running == " << running << "\n";
+            size_t new_pos = text.find(sep, cur);
+            sep_pos_dict.erase(ps);
+            if (new_pos != string::npos)
+                sep_pos_dict[new_pos] = sep;
+        }
+    }
+    
+    return res;
 }
 // -----Parser-----------------------------------------------------------
 
-void print_help()
+void process_file(const filesystem::directory_entry de, ofstream& out_f, mutex& mtx)
 {
-	cout << "elvis_neotek <input-folder> <output-file>\n";
+    {
+        lock_guard<mutex> lg(mtx);
+        cout << "thread " << this_thread::get_id() << " started" << endl;
+    }
+
+    if (!de.is_regular_file())
+        return;
+
+    ifstream in_f(de.path());
+    if (!in_f)
+    {
+        lock_guard<mutex> lg(mtx);
+        cerr << "ERROR: can't open file for reading: " << de << endl;
+        return;
+    }
+
+    Parser parser;
+
+    string line;
+    getline(in_f, line);
+    parser.add_text(line);
+
+    while (in_f)
+    {
+        string s;
+        getline(in_f, s);
+        parser.add_sep(s);
+    }
+
+    vector<string> res = parser.parse();
+    {
+        lock_guard<mutex> lg(mtx);
+
+        out_f << "[" << de.path().filename() << "]:" << endl;
+        for (const string& str : res)
+            out_f << str << endl;
+
+        cout << "thread " << this_thread::get_id() << " finished" << endl;
+    }
 }
 
-
-
-int main(int argc, char** argv)
+void parallel_process(const filesystem::path &in_folder, ofstream &out_f)
 {
-    if (argc != 3)
-	{
-		cerr << "ERROR: 2 parameters should be passed" << endl;
-		print_help();
-		return EINVAL;
-	}
+    unsigned thr_num = thread::hardware_concurrency();
+    vector<future<void>> tasks(thr_num);
+    mutex mtx;
+    // TODO | maybe we can just create as many tasks as we wish and they will be executed in cores number batches
 
-	filesystem::path in_folder(argv[1]);
-	if (!filesystem::exists(in_folder))
-	{
-		cerr << "ERROR: input folder doesn't exist" << endl;
-		return ENOENT;
-	}
+    auto file_iter = filesystem::directory_iterator(in_folder);
+    auto file_iter_end = end(filesystem::directory_iterator(in_folder));
 
-	ofstream out_f(argv[2]);
-	if (!out_f)
-	{
-		cerr << "ERROR: can't create output file" << endl;
-		return EIO;
-	}
+    bool started = false;
+    int running = 0;
+    while (!started || running > 0)
+    {
+        for (unsigned i = 0; i < thr_num; ++i)
+            if (!tasks[i].valid())
+            {
+                if (file_iter != file_iter_end)
+                {
+                    tasks[i] = async(launch::async, process_file, *file_iter, ref(out_f), ref(mtx));
+                    ++file_iter;
+                    ++running;
+                    started = true;
+                }
+            }
+            else if (tasks[i].wait_for(0ms) == future_status::ready)
+            {
+                if (file_iter != file_iter_end)
+                {
+                    tasks[i] = async(launch::async, &process_file, *file_iter, ref(out_f), ref(mtx));
+                    ++file_iter;
+                }
+                else
+                {
+                    tasks[i] = future<void>();
+                    --running;
+                }
+            }
+        this_thread::sleep_for(100ms);
+    }
 
-	Parser parser;
-	parser.parallel_process(in_folder, out_f);
-	return 0;
+    cout << "running == " << running << endl;
 }
--- elvis/elvis_neotek.hpp
+++ elvis/elvis_neotek.hpp
@@ -1,3 +1,26 @@
-#pragma once
+#ifndef ELVIS_NEOTEK_H
+#define ELVIS_NEOTEK_H
 
-// void parallelProcess(std::filesystem::path &in_folder, std::ofstream &out_f);
+#include <map>
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <fstream>
+#include <mutex>
+
+class Parser
+{
+public:
+	void add_text(const std::string& text);
+	void add_sep(const std::string& sep);
+	std::vector<std::string> parse();
+
+private:
+	std::string text;
+	std::vector<std::string> separators;
+};
+
+void process_file(const std::filesystem::directory_entry de, std::ofstream& out_f, std::mutex& mtx);
+void parallel_process(const std::filesystem::path &in_folder, std::ofstream &out_f);
+
+#endif

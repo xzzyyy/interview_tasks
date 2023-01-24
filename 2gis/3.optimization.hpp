db55609c328c40feac37c06f8b5b876d698b8a58
Pavel S <xzzyyy1@gmail.com>
2023-01-24 21:44:02 +0000

    2gis > optimization > speed

--- 2gis/3.optimization.hpp
+++ 2gis/3.optimization.hpp
@@ -2,6 +2,7 @@
 #define _3_OPTIMIZATION_HPP
 
 #include <vector>
+#include <map>
 #include <random>
 #include <numeric>
 #include <execution>
@@ -9,9 +10,43 @@
 #include <mutex>		// mutex
 #include <chrono>		// chrono::milliseconds
 #include <limits>		// numeric_limits<...>
+#include <stdexcept>	// overflow_error
 
 struct Optimization
 {
+	struct Gen
+	{
+		Gen(int count)
+		{
+			int last_idx = 0;
+			for (int i = 0; i < count; ++i)
+			{
+				sub_arr_beg[last_idx] = i;
+				last_idx += func(i) ? count + 1 : 1;
+			}
+		}
+		int operator()(int res_idx)
+		{
+			int size = prev(sub_arr_beg.cend())->second + static_cast<int>(sub_arr_beg.size());
+			if (res_idx < 0)
+				throw std::underflow_error(std::string("required idx [") + std::to_string(res_idx) + 
+										   "] is less than 0");
+			if (res_idx >= size)
+				throw std::overflow_error(std::string("required idx [") + std::to_string(res_idx) + 
+										  "] is more than total array size [" + std::to_string(size) + "]");
+			
+			int idx = sub_arr_beg.lower_bound(res_idx)->second;
+			int sub_idx = res_idx - idx;
+			
+			if (sub_idx == 0)
+				return idx;
+			else
+				return (sub_idx - 1) * idx;
+		}
+	private:
+		std::map<int, int> sub_arr_beg;
+	};
+
 	// it's not specified if `func` is it pure (чистая) function, or not, so we must treat it as not pure
 	static bool func(int i)
 	{
@@ -56,17 +91,13 @@ struct Optimization
         return res;
 	}
 
-	// std::vector<int> generator_memory(int idx)
-	// {
-	// 	static std::vector<bool> func_value;
-		
-	// 	// ?
-		
-	// 	return result;
-	// }
+	Gen generator_space(const int count)
+	{
+		return Gen(count);
+	}
 };
+
+#endif
 
 // для каких бы значений ты бы написал юнит-тесты?
 // 0, 1, max_value, -1
-
-#endif

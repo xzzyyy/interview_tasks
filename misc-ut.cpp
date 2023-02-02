fd791fc0152909b430556ce4b3a26b8f6caebc34
Pavel S <xzzyyy1@gmail.com>
2023-02-02 17:26:47 +0000

    moved hourly reporting table to diary

--- misc-ut.cpp
+++ misc-ut.cpp
@@ -6,13 +6,13 @@ using namespace std;
 BOOST_AUTO_TEST_CASE(pid)
 {
     auto pid = PeakMemory::this_process_id();
-    BOOST_TEST(pid != 0, "pid != 0");
-    BOOST_TEST(pid < 100'000'000, "pid < 100'000'000");
+    BOOST_TEST(pid != 0u, "pid != 0u");
+    BOOST_TEST(pid < 100'000'000u, "pid < 100'000'000u");
 }
 
 BOOST_AUTO_TEST_CASE(process_mem, * boost::unit_test::timeout(2))
 {
     auto peak_mem = PeakMemory::mega();
     BOOST_TEST(peak_mem > 0.1, "peak_mem > 100KB");
-    BOOST_TEST(peak_mem < 100, "peak_mem < 100MB");
+    BOOST_TEST(peak_mem < 100.0, "peak_mem < 100MB");
 }

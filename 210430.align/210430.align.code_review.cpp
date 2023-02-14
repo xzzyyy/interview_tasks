2d153b7e5cf5a5ca1b2e6d43d9f1a6230f183535
Pavel S <xzzyyy1@gmail.com>
2023-02-14 16:08:34 +0000

    Align Tec. code review task improvements

--- 210430.align/210430.align.code_review.cpp
+++ 210430.align/210430.align.code_review.cpp
@@ -3,67 +3,65 @@
 template<class T>
 class 3DVector								// identifier can't start with number
 {
-    T* d_data;
- 
+    T* d_data;								// it's better not to use pointers (we can use containers or smart pointers)
+											// public interface first, private part after visible part
+
 public:
-    3DVector() : d_data(0)					// no function body {}
-											// shouldn't we specify nullptr_t instead of 0?
-											// if we will have d_data initialized like that, all below functions will cause segfault
- 
-    template<class S>
-    3DVector(const S& v1, const S& v2, const S& v3)
-    {
-        d_date = new S(3);					// should be new S[3]
-											// d_date should be d_data
-											// shouldn't parameters be of type const T&?
+    3DVector() : d_data(0)					// probably author meant deleted default constructor
+											// member initializer list, but no constructor body `{}`
+											// `nullptr_t` instead of 0?
+
+    template<class S>						// member function can't be templated
+    3DVector(const S& v1, const S& v2, const S& v3)		// missing constructor (for ex. from 3 `T` arguments)
+    {									// better variable names possible (for ex. use x, y, z instead of v1, v2, v3)
+        d_date = new S(3);					// typo in identifier
+											// wrong array-new / array-delete syntax
         d_data[0] = v1;
         d_data[1] = v2;
         d_data[2] = v3;
     }
  
-    void operator=(const 3DVector& v)		// operator= should return Vector3D&
+    void operator=(const 3DVector& v)		// wrong operator signature
     {
         d_data[0] = v[0];
         d_data[1] = v[1];
         d_data[2] = v[2];
     }
  
-    T operator[](int i) { return d_data[i]; }
-											// should return T&
-											// we should also add const T& operator[](int i) const
+    T operator[](int i) { return d_data[i]; }		// wrong operator signature
+											// missing `const` operator versions
  
-    void operator+(const 3DVector& v)		// Did author mean operator+=? Should have Vector3D& as return type then
+    void operator+(const 3DVector& v)		// wrong operator signature
     {
         d_data[0] += v[0];
         d_data[1] += v[1];
         d_data[2] += v[2];
     }
    
-    void operator*(T v)						// Did author mean operator*=? Should have Vector3D& as return type then
+    void operator*(T v)						// wrong operator signature
     {
         d_data[0] *= v;
         d_data[1] *= v;
         d_data[2] *= v;
     }
    
-    T operator*(const 3DVector& v)			// function should be const
+    T operator*(const 3DVector& v)			// member function can be `const`
     {
         return d_data[0] * v[0] + d_data[1] * v[1] + d_data[2] * v[2];
     }
    
-    3DVector<T> operator^(const 3DVector& v)
-											// function should be const
+    3DVector<T> operator^(const 3DVector& v)		// member function can be `const`
     {
         3DVector result;					
         result[0] = d_data[1] * v[2] - d_data[2] * v[1];
         result[1] = d_data[0] * v[2] - d_data[2] * v[0];
         result[2] = d_data[0] * v[1] - d_data[1] * v[0];
-        return std::move(result);			// std::move is not needed, because return statement provides copy elision
+        return std::move(result);			// copy elision (`move` in `return` statement redundant)
     }
  
 private:
-    ~3DVector()								// private destructor, should we really use it?
+    ~3DVector()								// destructor should be `public`
     {
-      delete d_data;						// we should use delete[] as d_data is array
+        delete d_data;						// wrong array-new / array-delete syntax
     }
 }
--- 210430.align/210430.align.code_review.fixed.cpp
+++ 210430.align/210430.align.code_review.fixed.cpp
@@ -1,80 +1,65 @@
 #include <iostream>
-using namespace std;
+#include <array>
 
 template<class T>
 class Vector3D
 {
-    T* d_data;
- 
 public:
-    Vector3D() : d_data(0)				    // no function body {}
-                                            // shouldn't we specify nullptr_t instead of 0?
-											// if we will have d_data initialized like that, all below functions will cause segfault
-	{
-	    d_data = new T[3];
-	    memset(d_data, sizeof(d_data), 0);
-	}										
- 
-    Vector3D(const T& v1, const T& v2, const T& v3)
+    Vector3D() = delete;
+
+    Vector3D(const T& x, const T& y, const T& z)
     {
-        d_data = new T[3];					// should be new S[3]
-											// d_date should be d_data
-											// shouldn't parameters be of type const T&?
-        d_data[0] = v1;
-        d_data[1] = v2;
-        d_data[2] = v3;
+        coords[0] = x;
+        coords[1] = y;
+        coords[2] = z;
     }
  
-    Vector3D& operator=(const Vector3D& v)  // operator= should return 3DVector&
+    Vector3D& operator=(const Vector3D& v)
     {
-        d_data[0] = v[0];
-        d_data[1] = v[1];
-        d_data[2] = v[2];
+        coords[0] = v[0];
+        coords[1] = v[1];
+        coords[2] = v[2];
     }
  
-    T& operator[](int i) { return d_data[i]; }
-    const T& operator[](int i) const { return d_data[i]; }
-											// should return T&
-											// we should also add const T& operator[](int i) const
+    T& operator[](int i) { return coords[i]; }
+    const T& operator[](int i) const { return coords[i]; }
  
-    Vector3D operator+=(const Vector3D& v)  // Did author mean operator+=? Should have 3DVector& as return type then
+    Vector3D& operator+=(const Vector3D& v)
     {
-        d_data[0] += v[0];
-        d_data[1] += v[1];
-        d_data[2] += v[2];
+        coords[0] += v[0];
+        coords[1] += v[1];
+        coords[2] += v[2];
     }
    
-    Vector3D& operator*=(T v)				// Did author mean operator*=? Should have 3DVector& as return type then
+    Vector3D& operator*=(const T& v)
     {
-        d_data[0] *= v;
-        d_data[1] *= v;
-        d_data[2] *= v;
+        coords[0] *= v;
+        coords[1] *= v;
+        coords[2] *= v;
     }
    
-    T operator*(const Vector3D& v)
+    T operator*(const Vector3D& v) const
     {
-        return d_data[0] * v[0] + d_data[1] * v[1] + d_data[2] * v[2];
+        return coords[0] * v[0] + coords[1] * v[1] + coords[2] * v[2];
     }
    
     Vector3D operator^(const Vector3D& v) const  
-                                            // function should be const
     {
         Vector3D result;
-        result[0] = d_data[1] * v[2] - d_data[2] * v[1];
-        result[1] = d_data[0] * v[2] - d_data[2] * v[0];
-        result[2] = d_data[0] * v[1] - d_data[1] * v[0];
-        return result;			            // std::move is not needed, because return statement provides copy elision
+        result[0] = coords[1] * v[2] - coords[2] * v[1];
+        result[1] = coords[0] * v[2] - coords[2] * v[0];
+        result[2] = coords[0] * v[1] - coords[1] * v[0];
+        return result;
     }
 
-    ~Vector3D()							    // private destructor, should we really use it?
-											// 3DVector<T>
-    {
-        delete d_data;						// we should use delete[] as d_data is array
-    }
+private:
+    std::array<T, 3> coords;
 };
 
 int main()
 {
-    Vector3D<int> int_vector(1, 2, 3);
-    cout << (int_vector * int_vector) << endl;
+    Vector3D<int> i_vec(1, 2, 3);
+    Vector3D<double> d_vec(1.1, 2.2, 3.3);
+    std::cout << (i_vec * i_vec) << std::endl;
+    std::cout << (d_vec * d_vec) << std::endl;
 }

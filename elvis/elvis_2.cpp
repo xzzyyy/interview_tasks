da3d08d123c66ed5b3c5c4bd2ef2105c64e1670d
Pavel S <xzzyyy1@gmail.com>
2023-04-19 00:53:12 +0300

    yandex .cpp renamed

--- elvis/elvis_2.cpp
+++ elvis/elvis_2.cpp
@@ -4,8 +4,8 @@
 
 
 int do_foo() {
-    Foo* foo = new Foo();       // Foo created dynamically, not freed and pointer is lost after returning
-    foo->Init();
+    Foo* foo = new Foo();       // Foo created dynamically, and its resources will never be released
+    foo->Init();                // We can use automatic storage duration (stack) instance of Foo
     foo->Preprocess();
     foo->Calculate();
     return foo->GetResult();

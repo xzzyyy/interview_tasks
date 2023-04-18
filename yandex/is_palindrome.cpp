48d0daa314376597f24199e135a91ef0b641cd94
Pavel S <xzzyyy1@gmail.com>
2023-04-19 01:03:03 +0300

    yandex interview _is palindrome_ task

--- yandex/is_palindrome.cpp
+++ yandex/is_palindrome.cpp
@@ -1,48 +1,64 @@
-// ""
-// "a"
-// "aga"
-// "agb"
-// ",,,"
-// ",,."
-// ",a,"
-// ",,,aaa"
-// ".,aa.b,a.a"
-
 // Palindrome
 
-bool isalphanum(char c);
-char tolower(char c);
+#include <string>
+#include <cctype>
+
+#define BOOST_TEST_MODULE test_yandex
+#include <boost/test/included/unit_test.hpp>
 
 bool IsPalindrome(const std::string& str) {
     size_t len = str.length();
-    
+
     if (len == 0)
         return true;
     
     size_t l = 0;
     size_t r = len - 1;
-    
+
     do
     {
-        while (l < r && !isalphanum(str[l]))
+        while (l < r && !std::isalnum(static_cast<unsigned char>(str[l])))
             ++l;
-        while (l < r && !isalphanum(str[r]))
+        while (l < r && !std::isalnum(static_cast<unsigned char>(str[r])))
             --r;
         if (l == r)
             return true;
-        
-        if (tolower(str[l]) != tolower(str[r]))
+
+        if (std::tolower(static_cast<unsigned char>(str[l])) != std::tolower(static_cast<unsigned char>(str[r])))
             return false;
-            
+
         if (r - l <= 2)
             break;
-          
+
         ++l;
         --r;
     }
     while (true);
-    
+
     return true;
+}
+
+// ""               true
+// "a"              true
+// "aga"            true
+// "agb"            false
+// ",,,"            true
+// ",,."            true
+// ",a,"            true
+// ",,,aaa"         true
+// ".,aa.b,a.a"     true
+
+BOOST_AUTO_TEST_CASE(is_palindrome)
+{
+    BOOST_TEST(IsPalindrome(""));
+    BOOST_TEST(IsPalindrome("a"));
+    BOOST_TEST(IsPalindrome("aga"));
+    BOOST_TEST(!IsPalindrome("agb"));
+    BOOST_TEST(IsPalindrome(",,,"));
+    BOOST_TEST(IsPalindrome(",,."));
+    BOOST_TEST(IsPalindrome(",a,"));
+    BOOST_TEST(IsPalindrome(",,,aaa"));
+    BOOST_TEST(IsPalindrome(".,aa.b,a.a"));
 }
 
 // Equiilibrium Point
@@ -51,7 +67,7 @@ bool IsPalindrome(const std::string& str) {
 // { 0 }
 // { 1 1 1 }
 
-int GetEquilibriumPoint(const std::vector<int>& numbers) {
+/*int GetEquilibriumPoint(const std::vector<int>& numbers) {
     size_t size = numbers.size();
     
     if (size == 0)
@@ -82,4 +98,4 @@ int GetEquilibriumPoint(const std::vector<int>& numbers) {
     }
     
     throw;
-}
+}*/
